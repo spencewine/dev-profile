d672356eee3903067e08a808a07ff90dac9d5266
Spencer Goodwine <sdgoodwine@gmail.com>
2019-06-20 20:53:41 -0500

    first commit

---
+++ dev-profile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dev-profile
+//
+//  Created by Spencer Goodwine on 6/20/19.
+//  Copyright © 2019 Spencer Goodwine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
